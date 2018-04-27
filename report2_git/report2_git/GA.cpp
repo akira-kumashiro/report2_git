a011ba14588e5c3265d4585c5d39804e34efd167
Akira Kumashiro <34697080+akira-kumashiro@users.noreply.github.com>
2018-04-27 10:13:24 +0900

    Add files via upload

---
+++ report2_git/report2_git/GA.cpp
@@ -0,0 +1,272 @@
+#include "stdafx.h"
+#include "GA.h"
+
+GA::GA(int _max_genom_list, int _var_num, std::vector<double> _varMax, std::vector<double> _varMin) :
+	data(std::vector<Data>(_max_genom_list, _var_num)),//dataの初期化
+	eliteData(_var_num)
+{
+	//もらった変数をクラス内変数に格納
+	max_genom_list = _max_genom_list;
+	var_num = _var_num;
+	varMax = _varMax;
+	varMin = _varMin;
+}
+
+bool GA::init()
+{
+	for (int i = 0; i < max_genom_list; i++)
+	{
+		for (int j = 0; j < var_num; j++)
+		{
+			data[i].x[j] = random(varMin[j], varMax[j]);//遺伝子の初期設定
+			printf_s("%lf", data[i].x[j]);
+		}
+		data[i].calcResult(varMax, varMin);//評価関数
+		resultSumValue += data[i].result;//評価関数の合計を計算
+		printf_s(" \t Result=%7.4lf\n", data[i].result);
+	}
+	return true;
+}
+
+bool GA::selection()
+{
+	int max_num = 0;//最も評価の良い個体の番号
+	bool ret = false;
+
+	calc(false);
+
+	prev_data = data;
+
+	resultSumValue = 0;
+	for (int i = 0; i < max_genom_list; i++)
+		//ルーレット選択用に評価関数の合計と一番評価の良い番号を取得
+	{
+		resultSumValue += prev_data[i].result;
+		if (prev_data[i].result > prev_data[max_num].result)
+			max_num = i;
+	}
+
+	eliteData = prev_data[max_num];//データの先頭は最も評価の良い個体
+								   //	eliteData = prev_data[minNum];
+	if (eliteData.result - prev_data[minNum].result >= 1)//最も評価の良い個体の変化の監視(デバッグ用)
+		ret = true;
+
+	for (int i = 0; i < max_genom_list; i++)
+	{
+		double selector = random(0.0, 1.0);//乱数を生成
+		double needle = 0;//ルーレットの針を生成
+		int j = 0;
+		for (;; j++)
+		{
+			needle += (prev_data[j].result / resultSumValue);//ルーレットの針を乱数の値まで進める
+			if (needle > selector)
+				break;
+			if (j == (max_genom_list - 1))
+				break;
+		}
+		data[i] = prev_data[j];
+	}
+	return ret;
+}
+
+/*bool GA::uniformityCrossover()
+{
+	prev_data = data;
+
+	for (int i = 0; i < max_genom_list; i += 2)//2個ずつ交叉
+	{
+		for (int j = 0; j < var_num; j++)
+		{
+			bool isCrossover = (random(0.0, 1.0) >= crossoverRate ? true : false);//trueで交叉なし
+			data[i + 1].x[j] = isCrossover ? prev_data[i + 1].x[j] : prev_data[i].x[j];
+			//if (i != 0)//先頭のデータは保護
+			data[i].x[j] = isCrossover ? prev_data[i].x[j] : prev_data[i + 1].x[j];
+		}
+	}
+	return true;
+}
+
+bool GA::onePointCrossover()
+{
+	prev_data = data;
+
+	for (int i = 0; i < max_genom_list; i += 2)//2個ずつ交叉
+	{
+		int del1 = random(0, var_num - 1);
+		if (random(0.0, 1.0) <= crossoverRate)
+		{
+			for (int j = 0; j < del1; j++)
+			{
+				data[i + 1].x[j] = prev_data[i].x[j];
+				data[i].x[j] = prev_data[i + 1].x[j];
+			}
+		}
+		if (random(0.0, 1.0) <= crossoverRate)
+		{
+			for (int j = del1; j < var_num; j++)
+			{
+				data[i + 1].x[j] = prev_data[i].x[j];
+				data[i].x[j] = prev_data[i + 1].x[j];
+			}
+		}
+	}
+	return true;
+}
+
+bool GA::twoPointCrossover()
+{
+	prev_data = data;
+
+	for (int i = 0; i < max_genom_list; i += 2)//2個ずつ交叉
+	{
+		int del1 = random(0, var_num / 2);
+		int del2 = random(del1, var_num - 1);
+		if (random(0.0, 1.0) <= crossoverRate)
+		{
+			for (int j = 0; j < del1; j++)
+			{
+				data[i + 1].x[j] = prev_data[i].x[j];
+				data[i].x[j] = prev_data[i + 1].x[j];
+			}
+		}
+		if (random(0.0, 1.0) <= crossoverRate)
+		{
+			for (int j = del1; j < del2; j++)
+			{
+				data[i + 1].x[j] = prev_data[i].x[j];
+				data[i].x[j] = prev_data[i + 1].x[j];
+			}
+		}
+
+		if (random(0.0, 1.0) <= crossoverRate)
+		{
+			for (int j = del2; j < var_num; j++)
+			{
+				data[i + 1].x[j] = prev_data[i].x[j];
+				data[i].x[j] = prev_data[i + 1].x[j];
+			}
+		}
+	}
+	return true;
+}
+
+bool GA::tsunoPointCrossover()
+{
+	prev_data = data;
+
+	for (int i = 0; i < max_genom_list; i += 2)//2個ずつ交叉
+	{
+		int del1 = random(0, var_num - 1);
+		int del2 = random(del1, var_num);
+		if (random(0.0, 1.0) <= crossoverRate)
+		{
+			for (int j = del1; j < del2; j++)
+			{
+				data[i + 1].x[j] = prev_data[i].x[j];
+				data[i].x[j] = prev_data[i + 1].x[j];
+			}
+		}
+	}
+
+	return true;
+}*/
+
+bool GA::blxAlphaCrossover()
+{
+	prev_data = data;
+
+	for (int i = 0; i < max_genom_list; i += 2)//2個ずつ交叉
+	{
+		for (int j = 0; j < var_num; j++)
+		{
+			bool isCrossover = (random(0.0, 1.0) >= crossoverRate ? true : false);//trueで交叉なし
+			std::vector<double> range(var_num);
+			for (int k = 0; k < var_num; k++)
+			{
+				range[k] = std::abs(data[i].x[k] - data[i + 1].x[k]);
+			}
+			data[i + 1].x[j] = data[i];
+			data[i].x[j] = ;
+		}
+	}
+	return true;
+}
+
+bool GA::mutation()
+{
+	for (int i = 0; i < max_genom_list; i++)
+	{
+		if (random(0.0, 1.0) <= individualMutationRate)//個体突然変異率の計算
+		{
+			for (int j = 0; j < var_num; j++)
+			{
+				data[i].x[j] = random(varMin[j], varMax[j]);
+			}
+		}
+	}
+	return true;
+}
+
+bool GA::calc(bool enableDisplay)
+{
+	for (int i = 0; i < max_genom_list; i++)
+	{
+		data[i].calcResult(varMax, varMin);//評価関数の計算
+		if (data[i].result < data[minNum].result)
+		{
+			minNum = i;
+		}
+		if (data[i].result > data[maxNum].result)
+		{
+			maxNum = i;
+		}
+	}
+	data[minNum] = eliteData;
+
+	if (enableDisplay)
+	{
+		for (int i = 0; i < max_genom_list; i++)
+		{
+			for (int j = 0; j < var_num; j++)
+			{
+				printf_s("%d", data[i].x[j] ? 1 : 0);//デバッグ用
+			}
+			printf_s(" \t Result=%.4lf\n", data[i].result);
+		}
+	}
+	return true;
+}
+
+int GA::random(int min, int max)
+{
+	//乱数の設定
+	std::random_device rnd;
+	std::mt19937 engine(rnd());
+	std::uniform_int_distribution<int> distribution(min, max);
+	return distribution(engine);
+}
+
+double GA::random(int min, double max)
+{
+	return random((double)min, max);
+}
+
+double GA::random(double min, int max)
+{
+	return random(min, (double)max);
+}
+
+double GA::random(double min, double max)
+{
+	std::random_device rnd;
+	std::mt19937 engine(rnd());
+	std::uniform_real_distribution<double> distribution(min, max);
+	return distribution(engine);
+}
+
+
+
+GA::~GA()
+{
+
+}
---
+++ report2_git/report2_git/GA.h
@@ -0,0 +1,79 @@
+#pragma once
+
+#include<vector>
+#include <string>
+#include <iostream>
+#include <random>
+#include <algorithm>
+#include <cmath>
+
+class GA
+{
+private:
+	int max_genom_list;//個体数
+	int var_num;//品物の個数
+	double crossoverRate = 0.8;//交叉率
+	double individualMutationRate = 0.3;//個体突然変異率
+	double genomMutaionRate = 0.04;//遺伝子突然変異率
+	int minNum = 0, maxNum = 0;
+	double alpha = 0.3;
+public:
+	std::vector<double> varMax, varMin;
+	double resultSumValue;//評価関数の合計
+
+	class Data//データ格納用クラス
+	{
+	private:
+		double coefficient = 0.1;//評価関数用の定数
+		int var_num;//変数の数
+	public:
+		std::vector<double> x;//品物を入れるかどうかの配列
+		double result;//評価関数の値
+
+		Data(int _var_num)//コンストラクタ
+		{
+			var_num = _var_num;
+
+			x.resize(var_num);//isIncludedの配列の長さの設定
+		}
+
+		double calcResult(std::vector<double> _varMax, std::vector<double> _varMin)//評価関数
+		{
+			bool flag = true;
+			for (int i = 0; i < var_num; i++)
+			{
+				if (x[i] > _varMax[i] || x[i] < _varMin[i])
+					flag = false;
+			}
+
+			result = std::sin(x[0] + x[1]) + std::pow((x[0] - x[1]), 2.0) - 1.5*x[0] + 2.5*x[1] + 1;
+			if (!flag)//最大重量を超えなければそのまま
+			{
+				result = coefficient / result;
+			}
+			return result;
+		}
+	};
+
+	std::vector<Data> data, prev_data;//操作前後で値を保持するために2個
+	Data eliteData;
+	GA(int _max_genom_list, int _var_num, std::vector<double> _varMax, std::vector<double> _varMin);	//コンストラクタ
+	bool init();//初期化
+	bool selection();//選択
+
+	/*bool uniformityCrossover();//交叉
+	bool onePointCrossover();
+	bool twoPointCrossover();
+	bool tsunoPointCrossover();*/
+	bool blxAlphaCrossover();
+	bool mutation();//突然変異
+	bool calc(bool enableDisplay);//評価関数の計算
+	int random(int min, int max);
+	double random(int min, double max);
+	double random(double min, int max);
+	double random(double min, double max);
+
+	~GA();//デコンストラクタ
+};
+
+
