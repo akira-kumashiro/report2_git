aeb7ae76759b75e5fb061ffb8846e8043dae0bff
Akira Kumashiro <34697080+akira-kumashiro@users.noreply.github.com>
2018-04-27 16:12:41 +0900

    動作版

--- report2_git/report2_git/GA.cpp
+++ report2_git/report2_git/GA.cpp
@@ -21,10 +21,12 @@ bool GA::init()
 			data[i].x[j] = random(varMin[j], varMax[j]);//遺伝子の初期設定
 			printf_s("%lf", data[i].x[j]);
 		}
-		data[i].calcResult(varMax, varMin);//評価関数
-		resultSumValue += data[i].result;//評価関数の合計を計算
-		printf_s(" \t Result=%7.4lf\n", data[i].result);
+		//data[i].calcResult(varMax, varMin);//評価関数
+		//resultSumValue += data[i].result;//評価関数の合計を計算
+		printf_s(" \t f(x,y)=%7.4lf\t Result=%9.5lf\n", data[i].functionValue, data[i].result);
 	}
+	prev_data = data;
+	calcResult();
 	return true;
 }
 
@@ -35,22 +37,21 @@ bool GA::selection()
 
 	calc(false);
 
-	prev_data = data;
-
-	resultSumValue = 0;
+	//resultSumValue = 0;
+	//calcResult();
 	for (int i = 0; i < max_genom_list; i++)
 		//ルーレット選択用に評価関数の合計と一番評価の良い番号を取得
 	{
-		resultSumValue += prev_data[i].result;
-		if (prev_data[i].result > prev_data[max_num].result)
+		//resultSumValue += prev_data[i].result;
+		if (data[i].result > data[max_num].result)
 			max_num = i;
 	}
 
-	eliteData = prev_data[max_num];//データの先頭は最も評価の良い個体
+	eliteData = data[max_num];//データの先頭は最も評価の良い個体
 								   //	eliteData = prev_data[minNum];
-	if (eliteData.result - prev_data[minNum].result >= 1)//最も評価の良い個体の変化の監視(デバッグ用)
+	if (-eliteData.functionValue + prev_data[max_num].functionValue != 0)//最も評価の良い個体の変化の監視(デバッグ用)
 		ret = true;
-
+	prev_data = data;
 	for (int i = 0; i < max_genom_list; i++)
 	{
 		double selector = random(0.0, 1.0);//乱数を生成
@@ -179,27 +180,33 @@ bool GA::blxAlphaCrossover()
 	{
 		for (int j = 0; j < var_num; j++)
 		{
-			double varL, varH;
+			double ave, length;//varL, varH, 
 
-			if (data[i].x[j] > data[i + 1].x[j])
+			/*if (data[i].x[j] > data[i + 1].x[j])
 			{
 				varH = data[i].x[j];
-				varL = data[i+1].x[j];
+				varL = data[i + 1].x[j];
 			}
 			else
 			{
 				varH = data[i + 1].x[j];
 				varL = data[i].x[j];
-			}
+			}*/
+			ave = (data[i].x[j] + data[i + 1].x[j]) / 2;
+			//length = varH - varL;
+			length = std::abs((data[i].x[j] - data[i + 1].x[j]));
 
-			bool isCrossover = (random(0.0, 1.0) >= crossoverRate ? true : false);//trueで交叉なし
+			data[i].x[j] = random(ave - length * (1 + alpha*2) / 2, ave + length * (1 + alpha) / 2);
+			data[i + 1].x[j] = random(ave - length * (1 + alpha) / 2, ave + length * (1 + alpha) / 2);
+
+			/*bool isCrossover = (random(0.0, 1.0) >= crossoverRate ? true : false);//trueで交叉なし
 			std::vector<double> range(var_num);
 			for (int k = 0; k < var_num; k++)
 			{
 				range[k] = std::abs(data[i].x[k] - data[i + 1].x[k]);
 			}
 			data[i + 1].x[j] = data[i];
-			data[i].x[j] = ;
+			data[i].x[j] = ;*/
 		}
 	}
 	return true;
@@ -222,9 +229,10 @@ bool GA::mutation()
 
 bool GA::calc(bool enableDisplay)
 {
+	calcResult();
 	for (int i = 0; i < max_genom_list; i++)
 	{
-		data[i].calcResult(varMax, varMin);//評価関数の計算
+		//data[i].calcResult(varMax, varMin);//評価関数の計算
 		if (data[i].result < data[minNum].result)
 		{
 			minNum = i;
@@ -236,16 +244,52 @@ bool GA::calc(bool enableDisplay)
 	}
 	data[minNum] = eliteData;
 
+	calcResult();
 	if (enableDisplay)
 	{
 		for (int i = 0; i < max_genom_list; i++)
 		{
 			for (int j = 0; j < var_num; j++)
 			{
-				printf_s("%d", data[i].x[j] ? 1 : 0);//デバッグ用
+				printf_s("%lf,", data[i].x[j]);//デバッグ用
 			}
-			printf_s(" \t Result=%.4lf\n", data[i].result);
+			printf_s(" \t f(x,y)=%7.4lf\t Result=%7.4lf\n", data[i].functionValue, data[i].result);
 		}
+	}
+	return true;
+}
+
+bool GA::calcResult()
+{
+	int maxNum = 0;
+	double seg;
+	for (int i = 0; i < max_genom_list; i++)
+	{
+		data[i].functionValue = std::sin(data[i].x[0] + data[i].x[1]) + std::pow((data[i].x[0] - data[i].x[1]), 2.0) - 1.5*data[i].x[0] + 2.5*data[i].x[1] + 1;
+		if (data[maxNum].functionValue < data[i].functionValue)
+		{
+			maxNum = i;
+		}
+	}
+	seg = data[maxNum].functionValue;
+	resultSumValue = 0;
+
+	for (int i = 0; i < max_genom_list; i++)
+	{
+		bool flag = true;
+		double coefficient = 0.0001;//評価関数用の定数
+		for (int j = 0; j < var_num; j++)
+		{
+			if (data[i].x[j] > varMax[j] || data[i].x[j] < varMin[j])
+				flag = false;
+		}
+		data[i].result = std::pow((data[i].functionValue - seg), 2.0);
+
+		if (!flag)//最大重量を超えなければそのまま
+		{
+			data[i].result *= coefficient;
+		}
+		resultSumValue += data[i].result;
 	}
 	return true;
 }
--- report2_git/report2_git/GA.h
+++ report2_git/report2_git/GA.h
@@ -12,11 +12,11 @@ class GA
 private:
 	int max_genom_list;//個体数
 	int var_num;//品物の個数
-	double crossoverRate = 0.8;//交叉率
-	double individualMutationRate = 0.3;//個体突然変異率
-	double genomMutaionRate = 0.04;//遺伝子突然変異率
+	//double crossoverRate = 0.8;//交叉率
+	double individualMutationRate = 0.1;//個体突然変異率
+	//double genomMutaionRate = 0.04;//遺伝子突然変異率
 	int minNum = 0, maxNum = 0;
-	double alpha = 0.3;
+	double alpha = 0.5;
 public:
 	std::vector<double> varMax, varMin;
 	double resultSumValue;//評価関数の合計
@@ -24,11 +24,12 @@ public:
 	class Data//データ格納用クラス
 	{
 	private:
-		double coefficient = 0.1;//評価関数用の定数
+		//double coefficient = 0.01;//評価関数用の定数
 		int var_num;//変数の数
 	public:
 		std::vector<double> x;//品物を入れるかどうかの配列
-		double result;//評価関数の値
+		double functionValue;//評価関数の値
+		double result;
 
 		Data(int _var_num)//コンストラクタ
 		{
@@ -37,7 +38,7 @@ public:
 			x.resize(var_num);//isIncludedの配列の長さの設定
 		}
 
-		double calcResult(std::vector<double> _varMax, std::vector<double> _varMin)//評価関数
+		/*double calcResult(std::vector<double> _varMax, std::vector<double> _varMin)//評価関数
 		{
 			bool flag = true;
 			for (int i = 0; i < var_num; i++)
@@ -46,13 +47,13 @@ public:
 					flag = false;
 			}
 
-			result = std::sin(x[0] + x[1]) + std::pow((x[0] - x[1]), 2.0) - 1.5*x[0] + 2.5*x[1] + 1;
+			functionValue = std::sin(x[0] + x[1]) + std::pow((x[0] - x[1]), 2.0) - 1.5*x[0] + 2.5*x[1] + 1;
 			if (!flag)//最大重量を超えなければそのまま
 			{
-				result = coefficient / result;
+				functionValue = coefficient * functionValue;
 			}
-			return result;
-		}
+			return functionValue;
+		}*/
 	};
 
 	std::vector<Data> data, prev_data;//操作前後で値を保持するために2個
@@ -68,6 +69,7 @@ public:
 	bool blxAlphaCrossover();
 	bool mutation();//突然変異
 	bool calc(bool enableDisplay);//評価関数の計算
+	bool calcResult();
 	int random(int min, int max);
 	double random(int min, double max);
 	double random(double min, int max);
